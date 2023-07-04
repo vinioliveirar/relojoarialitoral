b1ff210a0a152cbcd3942a371696ab7f6fa193bd
Vinicius Oliveira Rocha <vinicius@Mac-mini-de-Stefany.local>
2023-07-04 14:29:36 -0300

    header styles

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import styles from "./header.module.scss";
+import logo from "../../assets/logoPNG.png";
 export default function Header() {
   return (
     <header className={styles.header}>
@@ -6,15 +8,25 @@ export default function Header() {
         <nav className={styles.Nav}>
           <ul className={styles.navList}>
             <li>
-              <a href="">Home</a>
+              <a href="">Início</a>
+            </li>
+            <li>
+              <a href="">Serviços</a>
             </li>
             <li>
               <a href="">Sobre</a>
             </li>
             <li>
-              <a href="">Entre em Contato</a>
+              <a href="">Solicite Cotação</a>
+            </li>
+            <li>
+              <a href="">Contatos</a>
             </li>
           </ul>
+
+          <div className={styles.logo}>
+            <Image alt="logo" src={logo} width={64} height={80} />
+          </div>
         </nav>
       </div>
     </header>
